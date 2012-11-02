a3fbe787550cbb7ed203e278224537c5d34a2566
SND\nsleightholm_cp <SND\nsleightholm_cp@1eac2b61-d0d7-4846-87ee-9410fe51cbb3>
2012-11-02 17:22:00 +0000

    Update dialogs.

--- inc/wixver.h
+++ inc/wixver.h
@@ -3,11 +3,11 @@
 #define _VERSION_FILE_H_
 
 #define szVerMajorMinor "3.6"
-#define szVerMajorMinorBuildRev "3.6.4676.38985"
+#define szVerMajorMinorBuildRev "3.6.4690.31126"
 #define rmj	3
 #define rmm	6
-#define rbd	4676
-#define rev	38985
+#define rbd	4690
+#define rev	31126
 #define szVerName	"BalExtensionExt Release"
 
 #endif
